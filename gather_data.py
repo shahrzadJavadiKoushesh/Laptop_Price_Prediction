195d6b4f794d4cc9b8d6f67afc8dbe7720c49f6d
Shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-07 16:45:29 +0330

    get first 24 laptops with selenium

--- gather_data.py
+++ gather_data.py
@@ -1,27 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-# create a requests session
-session = requests.Session()
+gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
+binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
+driver = webdriver.Firefox(firefox_binary=binary, executable_path=gecko + '.exe')
+driver.get("https://torob.com/search/?query=%D9%84%D9%BE%20%D8%AA%D8%A7%D9%BE")
 
-# make a GET request to the Torob website search page for "لپ تاپ"
-response = session.get("https://torob.com/search/?query=%D9%84%D9%BE%20%D8%AA%D8%A7%D9%BE")
+lap_top_names = []
+prices = []
+laptops = driver.find_elements(By.XPATH, "/html/body/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div")
 
-# create a BeautifulSoup object from the response content
-soup = BeautifulSoup(response.content, "html.parser")
+for laptop in laptops:
+    lap_top_names.append(laptop.find_element(By.XPATH, "./a/div/div/h2").text)
+    prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
 
-# find all the laptop elements on the page
-laptop_elements = soup.find_all("div", attrs={"class": "jsx-fa8eb4b3b47a1d18"})
-
-# loop through the laptop elements and extract the information
-for i, laptop in enumerate(laptop_elements[:1000]):
-    
-    #  get the laptop price
-    price = laptop.find("div", attrs={"class": "jsx-fa8eb4b3b47a1d18"})
-    
-    # get the laptop name
-    name = laptop.find("h2", attrs={"class": "jsx-fa8eb4b3b47a1d18 name"})
-    
-    # print the laptop information
-    print(f"Laptop {i}: {name} - {price}")
-print(len(laptop_elements))
+for i in range(len(lap_top_names)):
+    print("laptop" + str(i+1) + ":" + lap_top_names[i] + " " + prices[i])
+    print()
