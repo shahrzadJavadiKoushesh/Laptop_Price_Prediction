c8f0faf18c81873ac2865cd81cecdbd4aa923db3
Shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-09 18:42:25 +0330

    get rams

--- gather_data.py
+++ gather_data.py
@@ -12,7 +12,7 @@ binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 driver = webdriver.Firefox(firefox_binary=binary, executable_path=gecko + '.exe')
 driver.get("https://torob.com/search/?query=%D9%84%D9%BE%20%D8%AA%D8%A7%D9%BE")
 
-for i in range(120):
+for i in range(100):
     driver.execute_script("window.scrollBy(0, -500);")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
@@ -20,13 +20,23 @@ for i in range(120):
 
 laptop_names = []
 prices = []
+RAMs = []
 wait = WebDriverWait(driver, 10)
 laptops = wait.until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div")))
 
 for laptop in laptops:
-    laptop_names.append(laptop.find_element(By.XPATH, "./a/div/div/h2").text)
-    prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
+    name = laptop.find_element(By.XPATH, "./a/div/div/h2").text
+    if name.__contains__("|"):
+        laptop_names.append(name)
+        if name.__contains__("RAM") or name.__contains__("رم"):
+            ram = name.find("GB")
+            ram_capacity = name[ram-2] + name[ram-1]
+            RAMs.append(int(ram_capacity))
+            # print(ram)
+        prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
 
 for i in range(len(laptop_names)):
-    print("laptop" + str(i+1) + ":" + laptop_names[i] + " " + prices[i])
+    print("laptop " + str(i+1) + ":" + laptop_names[i] + " " + prices[i])
     print()
+
+print(RAMs)
