37d35c9bcc38f6f7f55397abdab570a05f118d2f
Shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-10 16:32:03 +0330

    cpu for core is

--- gather_data.py
+++ gather_data.py
@@ -29,16 +29,23 @@ for laptop in laptops:
     name = laptop.find_element(By.XPATH, "./a/div/div/h2").text
     laptop_names.append(name)
     # RAM info
-    ram = name.find("GB")
-    ram_capacity = name[ram - 2] + name[ram - 1]
-    if ram_capacity.isdigit() or any((char.isspace() and any(char.isdigit() for char in ram_capacity)) or (
-        char.isdigit() and any(char.isspace() for char in ram_capacity)) for char in ram_capacity):
-        RAMs.append(int(ram_capacity))
+    if name.__contains__("GB"):
+        ram = name.find("GB")
+        ram_capacity = name[ram - 2] + name[ram - 1]
+        if ram_capacity.isdigit() or any((char.isspace() and any(char.isdigit() for char in ram_capacity)) or (
+            char.isdigit() and any(char.isspace() for char in ram_capacity)) for char in ram_capacity):
+            RAMs.append(int(ram_capacity))
+    # CPU info
+    if name.__contains__(" i") or name.__contains__(" I"):
+        cpu = name.find(" i")
+        cpu_capacity = name[cpu+2]
+        CPUs.append("i"+str(cpu_capacity))
     else:
         RAMs.append("?")
-
+        CPUs.append("?")
     prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
 
 for i in range(len(laptop_names)):
     print("laptop " + str(i) + ":" + laptop_names[i] + " " + prices[i])
     print("RAM" + str(i) + ":" + str(RAMs[i]))
+    print("CPU" + str(i) + ":" + str(CPUs[i]))
