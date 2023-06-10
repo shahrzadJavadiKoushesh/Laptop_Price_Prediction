4941cd28cc7ff6c2f8ba347fcc985f2c2ae85d82
Shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-10 16:02:02 +0330

    get rams

--- gather_data.py
+++ gather_data.py
@@ -6,7 +6,6 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-
 gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
 binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
 driver = webdriver.Firefox(firefox_binary=binary, executable_path=gecko + '.exe')
@@ -15,28 +14,31 @@ driver.get("https://torob.com/search/?query=%D9%84%D9%BE%20%D8%AA%D8%A7%D9%BE")
 for i in range(100):
     driver.execute_script("window.scrollBy(0, -500);")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
-    print(i)
+    time.sleep(0.5)
+    print(i, end=" ")
 
 laptop_names = []
 prices = []
 RAMs = []
+CPUs = []
 wait = WebDriverWait(driver, 10)
-laptops = wait.until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div")))
+laptops = wait.until(EC.presence_of_all_elements_located(
+    (By.XPATH, "/html/body/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div/div")))
 
 for laptop in laptops:
     name = laptop.find_element(By.XPATH, "./a/div/div/h2").text
-    if name.__contains__("|"):
-        laptop_names.append(name)
-        if name.__contains__("RAM") or name.__contains__("رم"):
-            ram = name.find("GB")
-            ram_capacity = name[ram-2] + name[ram-1]
-            RAMs.append(int(ram_capacity))
-            # print(ram)
-        prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
+    laptop_names.append(name)
+    # RAM info
+    ram = name.find("GB")
+    ram_capacity = name[ram - 2] + name[ram - 1]
+    if ram_capacity.isdigit() or any((char.isspace() and any(char.isdigit() for char in ram_capacity)) or (
+        char.isdigit() and any(char.isspace() for char in ram_capacity)) for char in ram_capacity):
+        RAMs.append(int(ram_capacity))
+    else:
+        RAMs.append("?")
+
+    prices.append(laptop.find_element(By.XPATH, "./a/div/div/div[2]/div[2]").text)
 
 for i in range(len(laptop_names)):
-    print("laptop " + str(i+1) + ":" + laptop_names[i] + " " + prices[i])
-    print()
-
-print(RAMs)
+    print("laptop " + str(i) + ":" + laptop_names[i] + " " + prices[i])
+    print("RAM" + str(i) + ":" + str(RAMs[i]))
